1000be84c77424d0c9778c1204f89e65587a1eb7
Daniel Anestopoulos <dstops97@gmail.com>
2023-12-11 22:12:43 -0500

    Add Save+Edit

--- src/components/BasicInfo.jsx
+++ src/components/BasicInfo.jsx
@@ -1,5 +1,9 @@
 /* eslint-disable react/prop-types */
 import '/src/styles/BasicInfo.css'
+import Icon from '@mdi/react';
+import { mdiChevronUp } from '@mdi/js';
+import { mdiContentSave } from '@mdi/js';
+import { mdiPencilBoxOutline } from '@mdi/js';
 import { useState } from 'react'
 
 export default function BasicInfo({setGlobalInfo}) {
@@ -16,15 +20,58 @@ export default function BasicInfo({setGlobalInfo}) {
     setBasicInfo((prevInfo) => ({...prevInfo, [name]: value}))
   }
 
-  const handleSave =(e) => {
+  const handleSave = (e) => {
     e.preventDefault();
-    setGlobalInfo(basicInfo)
+    setGlobalInfo(basicInfo);
+    const buttons = document.querySelectorAll('.save-edit button');
+    buttons.forEach((button) => {
+      button.classList.toggle('hidden');
+    })
+    const inputs = document.querySelectorAll('#basicInfoForm .inputContainer');
+    inputs.forEach((input) => {
+      const inputField = input.querySelector('input');
+      if (inputField) {
+        inputField.disabled = true;
+    }});
+    
+  }
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+    const buttons = document.querySelectorAll('.save-edit button');
+    buttons.forEach((button) => {
+      button.classList.toggle('hidden');
+    })
+    const inputs = document.querySelectorAll('#basicInfoForm .inputContainer');
+    inputs.forEach((input) => {
+      const inputField = input.querySelector('input');
+      if (inputField) {
+        inputField.disabled = false;
+    }});
+  }
+
+  const toggleCollapse = () => {
+    const basicInfoForm = document.getElementById('basicInfoForm');
+    const chevron = document.querySelector('.infoHeader svg')
+    if (basicInfoForm.classList.contains('collapse')){
+      basicInfoForm.classList.remove('collapse');
+      chevron.classList.remove('down');
+      chevron.classList.add('up');
+    } else {
+      basicInfoForm.classList.add('collapse');
+      chevron.classList.remove('up');
+      chevron.classList.add('down');
+      
+    }
   }
 
   return (
     <div className='infoContainer'>
-      <h1>Personal Information</h1>
-      <form action="submit">
+      <div className="infoHeader" onClick={toggleCollapse}>
+        <h1>Personal Information</h1>
+        <Icon className='down' path={mdiChevronUp} size={1} />
+      </div>
+      <form action="submit" className='collapse' id='basicInfoForm'>
         <div className="inputContainer">
           <label htmlFor="fullName">Full Name</label>
           <input
@@ -58,7 +105,20 @@ export default function BasicInfo({setGlobalInfo}) {
           onChange={handleChange}
           />
         </div>
-        <button type='button' onClick={handleSave}>Save</button>
+        <div className="save-edit">
+          <button onClick={handleSave} >
+            <div>
+              <Icon path={mdiContentSave} size={1} />
+               <span>Save</span>
+            </div>
+          </button>
+          <button className='hidden' onClick={handleEdit}>
+            <div>
+              <Icon path={mdiPencilBoxOutline} size={1} />
+               <span>Edit</span>
+            </div>
+          </button>
+        </div>
       </form>
     </div>
   );
