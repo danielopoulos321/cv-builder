4b82fabe75aedd60f5a7162b64968b6a9e63275c
Daniel Anestopoulos <dstops97@gmail.com>
2023-12-15 14:54:22 -0500

    Cv education + career display

--- src/components/CVPreview.jsx
+++ src/components/CVPreview.jsx
@@ -3,9 +3,47 @@ import '/src/styles/CVPreview.css'
 export default function CVPreview({globalInfo}) {
     return(
         <div className="cvOutline">
-            <h1>{globalInfo.basicInfo.fullName}</h1>
-            <h2>{globalInfo.basicInfo.phone}</h2>
-            <h2>{globalInfo.basicInfo.email}</h2>
+            <div className="cvHeader">
+                <h1>{globalInfo.basicInfo.fullName}</h1>
+                <div className="contact">
+                    <h2>{globalInfo.basicInfo.phone}</h2>
+                    <h2>{globalInfo.basicInfo.email}</h2>
+                </div>
+            </div>
+            <div className="education">
+                <h1 className='subheading'>Education</h1>
+                <ul>
+                    {globalInfo.educationInfo.map((education) => (
+                        <li key={education.id} className='listEntry'>
+                            <div className='start-end'>
+                                <p>Start Date: {education.startDate}</p>
+                                <p>End Date: {education.endDate}</p>
+                            </div>
+                            <div className="details">
+                                <p><b>{education.schoolName}</b></p>
+                                <p>{education.degree}</p>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className="career">
+                <h1 className='subheading'>Professional Experience</h1>
+                <ul>
+                    {globalInfo.careerInfo.map((job) => (
+                        <li key={job.id} className='listEntry'>
+                            <div className='start-end'>
+                                <p>Start Date: {job.startDate}</p>
+                                <p>End Date: {job.endDate}</p>
+                            </div>
+                            <div className="details">
+                                <p><b>{job.employer}</b></p>
+                                <p>{job.title}</p>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
