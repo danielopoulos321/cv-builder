fa820206cf1408d7b2fa02dc5b9d42788e535db3
Daniel Anestopoulos <dstops97@gmail.com>
2023-12-15 13:54:50 -0500

    Remove duplicated styles

--- src/components/CVPreview.jsx
+++ src/components/CVPreview.jsx
@@ -3,9 +3,9 @@ import '/src/styles/CVPreview.css'
 export default function CVPreview({globalInfo}) {
     return(
         <div className="cvOutline">
-            <h1>{globalInfo.fullName}</h1>
-            <h2>{globalInfo.phone}</h2>
-            <h2>{globalInfo.email}</h2>
+            <h1>{globalInfo.basicInfo.fullName}</h1>
+            <h2>{globalInfo.basicInfo.phone}</h2>
+            <h2>{globalInfo.basicInfo.email}</h2>
         </div>
     )
 }
